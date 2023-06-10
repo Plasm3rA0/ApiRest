52e0d06caa43b2ea62e9b69db2ff227b578cdb50
polhernaan <phernancamino@insllica.cat>
2023-06-10 15:57:20 +0200

    DTO finished

---
+++ ApiRest/Controllers/userController.cs
@@ -0,0 +1,21 @@
+﻿using ApiRest.Models;
+using ApiRest.Models.Dto;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ApiRest.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class userController : ControllerBase
+    {
+        [HttpGet]
+        public IEnumerable<UserDto> GetVillas()
+        {
+            return new List<UserDto> {
+                new UserDto { Id = 1, Name="Pol Hernan Camino"},
+                new UserDto { Id = 2, Name="Veronica Lainez Liso"}
+            };
+        }
+    }
+}
